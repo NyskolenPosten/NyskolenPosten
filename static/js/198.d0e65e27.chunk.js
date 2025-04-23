dd48ece37df81c0cfb4b8cfa3f86b91761d6a516
Mattis <mattis@MacBookPro.fritz.box>
2025-04-23 12:45:26 +0200

    Updates

---
+++ static/js/198.d0e65e27.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknyskolen_posten=self.webpackChunknyskolen_posten||[]).push([[198],{1198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.d0e65e27.chunk.js.map
