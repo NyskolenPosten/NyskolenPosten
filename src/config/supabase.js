ce7c162b41e42530a08ffd48432955d19c3587f9
Mattis <mattis@MacBookPro.lan>
2025-05-13 20:16:20 +0200

    Oppdatert env key

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://lucbodhuwimhqnvtmdzg.supabase.co'
-export const supabaseKey = process.env.SUPABASE_KEY
+export const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Hjelpefunksjon for å sjekke autentisering
