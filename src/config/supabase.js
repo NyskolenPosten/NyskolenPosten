2694023148217086c3212a4e2de1413c15865b10
Mattis <mattis@MacBookPro.lan>
2025-05-13 19:30:57 +0200

    Fikset deploy error.

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://lucbodhuwimhqnvtmdzg.supabase.co'
+export const supabaseKey = process.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Hjelpefunksjon for å sjekke autentisering
 export const sjekkAuth = async () => {
